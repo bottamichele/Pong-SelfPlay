2dce50b17f65aaaf3163707c95606f849d45f35a
Botta Michele <72565868+bottamichele@users.noreply.github.com>
2025-04-02 01:57:39 +0200

    Add other variants of DQN

--- dqn_agent.py
+++ dqn_agent.py
@@ -5,9 +5,9 @@ from torch.nn.functional import mse_loss
 from torch.optim import Adam
 
 from dqn_algorithms.policy import e_greedy_policy
-from dqn_algorithms.nn import DuelingDQN
-from dqn_algorithms.memory import PropPriorMemory
-from dqn_algorithms.q_methods import compute_q_ddqn
+from dqn_algorithms.nn import DQN, DuelingDQN
+from dqn_algorithms.memory import UniformMemory, PropPriorMemory
+from dqn_algorithms.q_methods import compute_q_dqn, compute_q_ddqn
 
 from agent import AgentTrain
 
@@ -15,9 +15,9 @@ from agent import AgentTrain
 # ========== CLASS Dueling Agent DQN Train =========
 # ==================================================
 
-class DuelingDQNAgentTrain(AgentTrain):
-    def __init__(self, target_total_step, mem_size, batch_size, update_target_steps, lr=10**-4, gamma=0.99, eps_init=1.0, eps_min=0.01, eps_decay=9.9*10**-6, device=tc.device("cuda" if tc.cuda.is_available() else "cpu")):
-        """Create new training Dueling DQN agent.
+class DQNAgentTrain(AgentTrain):
+    def __init__(self, target_total_step, mem_size, batch_size, update_target_steps, use_double=True, use_dueling=True, use_per=True, lr=10**-4, gamma=0.99, eps_init=1.0, eps_min=0.01, eps_decay=9.9*10**-6, device=tc.device("cuda" if tc.cuda.is_available() else "cpu")):
+        """Create new training DQN agent.
         
         Parameters
         --------------------
@@ -32,6 +32,15 @@ class DuelingDQNAgentTrain(AgentTrain):
 
         update_target_steps: int
             target network is upadeted every 'update_target_steps'
+
+        use_double: bool, optional
+            whether or not to use double q
+
+        use_dueling: bool, optional
+            whether or not to use dueling architecture
+
+        use_per: bool, optional
+            whether or not to use prioritized memory replay
 
         lr: float, optional
             learning rate
@@ -66,17 +75,21 @@ class DuelingDQNAgentTrain(AgentTrain):
         self.target_total_step = target_total_step
         self.total_states = 0
         self.batch_size = batch_size
-        self.memory = PropPriorMemory(mem_size, 12, device=self.device)
+        self.memory = PropPriorMemory(mem_size, 12, device=self.device) if use_per else UniformMemory(mem_size, 12, device=self.device)
         self.epsilon = eps_init
         self.epsilon_min = eps_min
         self.epsilon_decay = eps_decay
-        self.beta_decay = (1.0 - self.memory.beta) / target_total_step
         self.gamma = gamma
         self.update_target_steps = update_target_steps
+        self.use_per = use_per
+        self.use_double = use_double
+
+        if use_per:
+            self.beta_decay = (1.0 - self.memory.beta) / target_total_step
 
         #Model.
-        self.model = DuelingDQN(12, 3, 256, 1, 1, 1).to(self.device)
-        self.target_model = DuelingDQN(12, 3, 256, 1, 1, 1).to(self.device)
+        self.model = DuelingDQN(12, 3, 256, 1, 1, 1).to(self.device) if use_dueling else DQN(12, 3, 256, 2).to(self.device)
+        self.target_model = DuelingDQN(12, 3, 256, 1, 1, 1).to(self.device) if use_dueling else DQN(12, 3, 256, 2).to(self.device)
         self.optimizer = Adam(self.model.parameters(), lr=lr)
 
         self.update_target_model()
@@ -97,28 +110,38 @@ class DuelingDQNAgentTrain(AgentTrain):
 
         if len(self.memory) >= self.batch_size:
             #Sample mini-batch.
-            obs_b, action_b, reward_b, next_obs_b, next_obs_done_b, weight_b = self.memory.sample_batch(self.batch_size)
+            if not self.use_per:
+                obs_b, action_b, reward_b, next_obs_b, next_obs_done_b = self.memory.sample_batch(self.batch_size)
+            else:
+                obs_b, action_b, reward_b, next_obs_b, next_obs_done_b, weight_b = self.memory.sample_batch(self.batch_size)
 
             #Convertion.
             action_b = action_b.to(dtype=tc.int32)
             next_obs_done_b = next_obs_done_b.to(dtype=tc.int32)
 
             #Computet q values.
-            q, q_target = compute_q_ddqn(self.model, self.target_model, self.gamma, obs_b, action_b, reward_b, next_obs_b, next_obs_done_b)
+            if not self.use_double:
+                q, q_target = compute_q_dqn(self.model, self.gamma, obs_b, action_b, reward_b, next_obs_b, next_obs_done_b, self.target_model)
+            else:
+                q, q_target = compute_q_ddqn(self.model, self.target_model, self.gamma, obs_b, action_b, reward_b, next_obs_b, next_obs_done_b)
 
             #Compute loss and gradient.
-            loss = tc.mean((q_target - q).pow(2) * weight_b)
+            if not self.use_per:
+                loss = mse_loss(q, q_target)
+            else:
+                loss = tc.mean((q_target - q).pow(2) * weight_b)
         
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
 
             #Update priorities.
-            td_errors = tc.clamp(q_target - q, -1.0, 1.0)
-            self.memory.update_priorities(td_errors)
+            if self.use_per:
+                td_errors = tc.clamp(q_target - q, -1.0, 1.0)
+                self.memory.update_priorities(td_errors)
 
         self.epsilon = self.epsilon - self.epsilon_decay if self.epsilon > self.epsilon_min else self.epsilon_min
-        self.memory.beta += self.beta_decay
+        if self.use_per:    self.memory.beta += self.beta_decay
 
         if self.total_states % self.update_target_steps == 0:
             self.update_target_model()
@@ -143,6 +166,12 @@ class DuelingDQNAgentTrain(AgentTrain):
 
 def load_dueling_dqn_model(model_path):
     model = DuelingDQN(12, 3, 256, 1, 1, 1)
+    model.load_state_dict(tc.load(model_path))
+
+    return model
+
+def load_dqn_model(model_path):
+    model = DQN(12, 3, 256, 2)
     model.load_state_dict(tc.load(model_path))
 
     return model
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from agent import Agent
 from self_play import train
 from run_agent import run_agent
 
-from dueling_dqn_agent import DuelingDQNAgentTrain, load_dueling_dqn_model
+from dqn_agent import DQNAgentTrain, load_dueling_dqn_model, load_dqn_model
 
 USE_DUELING_DQN = True
 TRAIN = False
 
 if __name__ == "__main__":
     if USE_DUELING_DQN and TRAIN:
-        train(DuelingDQNAgentTrain(5_000_000, 750000, 64, 5000, eps_decay=3.96*10**-6), 6, 20, 10, 0.5, 20, 10)
+        train(DQNAgentTrain(5_000_000, 750000, 64, 5000, eps_decay=3.96*10**-6), 6, 20, 10, 0.5, 20, 10)
     elif USE_DUELING_DQN and not TRAIN:
         model = load_dueling_dqn_model("./models/dueling_dqn.pth")
         agent = Agent(model)
